4893484004f46c3d2679f975782eeddf1900dabb
Lenovo <2297598918@qq.com>
2021-04-30 21:36:20 +0800

    Cycle

--- src/Cycle.java
+++ src/Cycle.java
@@ -2,33 +2,24 @@ public class Cycle {
     public void play() {
         System.out.println("Cycle");
     }
-    public static void wheels(){
-        System.out.println("Cycle轮子数量是"+"num");
+
     }
-}
 class Unicycle extends Cycle{
     public void play(){
         System.out.println("Unicycle");
     }
-    public static void wheels(){
-        System.out.println("Unicycle轮子数量是"+"1");
+
     }
-}
 class Bicycle extends Cycle {
     public void play(){
 
         System.out.println("Bicycle");
     }
-    public static void wheels(){
-        System.out.println("Bicycle轮子数量是"+"2");
+
     }
-}
 class Tricycle extends Cycle {
     public void play(){
         System.out.println("Tricycle");
-    }
-    public static void wheels(){
-        System.out.println("Tricycle轮子数量是"+"3");
     }
 }
 
@@ -44,9 +35,7 @@ class Ride {
         ride(unicycle);
         ride(bicycle);
         ride(tricycle);
-        Unicycle.wheels();
-        Bicycle.wheels();
-        Tricycle.wheels();
+
 
     }
 }
